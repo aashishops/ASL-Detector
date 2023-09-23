d63ce491dcae38a62b60ef2f526b04f28b4987e6
aashishops <107553285+aashishops@users.noreply.github.com>
2023-09-23 18:51:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 import time
 import streamlit as st
 from streamlit_webrtc import WebRtcMode, VideoTransformerBase, webrtc_streamer
-st.set_page_config(page_title='ASL Detection')
+
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 
@@ -82,12 +82,12 @@ class HandTrackingTransformer(VideoTransformerBase):
         return np.array(data)
 
 def main():
-    st.title("American Sign Language Detection with Hand Tracking")
+    st.title("ASL Sign Language Detection with Hand Tracking")
 
     webrtc_ctx = webrtc_streamer(
         key="hand-tracking",
         mode=WebRtcMode.SENDRECV,
-        video_transformer_factory=HandTrackingTransformer,
+        video_processor_factory=HandTrackingTransformer,
         async_processing=True,
     )
 
