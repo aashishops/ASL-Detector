6afbb63555d655ecb0d9a830c3f8ca87b7134eff
aashishops <107553285+aashishops@users.noreply.github.com>
2023-09-23 18:41:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 import time
 import streamlit as st
 from streamlit_webrtc import WebRtcMode, VideoTransformerBase, webrtc_streamer
-
+st.set_page_config(page_title='Language Detection')
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 
